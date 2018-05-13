e25f6b1328fb7c0e6d078b0978c0b4058fc3ff64
ahn89it@gmail.com <ahn89it@gmail.com>
2018-05-14 05:35:33 +0900

    180513

--- ALL_CODE.py
+++ ALL_CODE.py
@@ -1,7 +1,11 @@
+#KOSPY_CODE : 코스피코드 목록 배열
+#KOSDAQ_CODE : 코스닥코드 목록 배열
+#ALL_CODE : 모든 종목 코드 배열
+
 import requests
 from bs4 import BeautifulSoup
 import re
-
+PI = 33
 KOSPY_url = "http://finance.daum.net/quote/all.daum?type=S&stype=P"
 KOSDAQ_url = "http://finance.daum.net/quote/all.daum?type=U&stype=Q"
 KOSPY_html = requests.get(KOSPY_url).text
@@ -13,8 +17,8 @@ KOSPY_list = KOSPY_soup.select('td a')
 KOSDAQ_list = KOSDAQ_soup.select('td a')
 
 #  종목 Code 배열로 저장 & a href내의 숫자 추출  기능
-KOSPY_CODE = [] #코스피코드 목록 배열
-KOSDAQ_CODE = [] #코스닥코드 목록 배열
+KOSPY_CODE = []
+KOSDAQ_CODE = []
 for tag in KOSPY_list:
     link = tag['href']
     matched = re.search(r'\d+', link)
@@ -27,14 +31,17 @@ for tag in KOSDAQ_list:
     code = matched.group(0)
     KOSDAQ_CODE.append(code)
 
-print(KOSPY_CODE)
-print(len(KOSPY_CODE))
-
-print(KOSDAQ_CODE)
-print(len(KOSDAQ_CODE))
 
 ALL_CODE = KOSPY_CODE + KOSDAQ_CODE
-print(ALL_CODE)
-print(len(ALL_CODE))
+
+if __name__ == "__main__":
+    print(KOSPY_CODE)
+    print(len(KOSPY_CODE))
+
+    print(KOSDAQ_CODE)
+    print(len(KOSDAQ_CODE))
+
+    print(ALL_CODE)
+    print(len(ALL_CODE))
 
 
--- test3.py
+++ test3.py
@@ -1,26 +1,73 @@
 # 데이터 받아서 배열에 집어넣기 기능
+# MAL_Function(x)
+# Now_Price_Function(x)
+# MAL_Line(x)
+# def err_MAL_Line(x)
+# err_MAL_Line(x)
+import ALL_CODE
 import requests
 from bs4 import BeautifulSoup
 
-url1 = "http://m.finance.daum.net/m/item/item_daily.daum?code=035720"+"&page=1"
-html1 = requests.get(url1).text
-soup1 = BeautifulSoup(html1, "html.parser")
-test1 = soup1.select('tbody td.idx')
-array1 = [test1[i].text for i in range (len(test1))]
-print (array1)
-for i in range(len(array1)):
-    array1[i] =array1[i].replace(",","")
-print(array1[1])
+
+# X페이지 10개의 종가들 배열에 Input 함수
+# 입력값은 X페이지 / 결과값은 X페이지의 종가들 배열 생성
+def MAL_Function(x):
+    url = "http://m.finance.daum.net/m/item/item_daily.daum?code=035720&page={0}".format(x)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    test = soup.select('tbody td.idx')
+    array = [test[i].text for i in range(len(test))]
+
+    for i in range(len(array)):
+        array[i] = int(array[i].replace(",", ""))
+
+    return array
+
+
+#종가 20개 모음 배열
+End_Price = MAL_Function(1) + MAL_Function(2)
+
+# 현재 가격 함수 / 입력값 : 종목코드값
+# 배열로 저장된 종목 코드 하나를 주소 Code에 대입하여 현재 가격 출력
+def Now_Price_Function(x):
+    url_test = "http://m.finance.daum.net/m/item/main.daum?code={0}".format(x)
+    input_html = requests.get(url_test).text
+    input_soup = BeautifulSoup(input_html, "html.parser")
+    Now_Price = input_soup.find("span", {"class":"price"}) #클래스 네임으로 현재가격 찾기
+    Now_Price = int(Now_Price.text.replace(",", "")) #text str형으로 변환후 , 제거 후 int형으로 변환
+    return  Now_Price
+
+#현재가 계산
+Now_Price = Now_Price_Function("035720")
+
+# X일 이동평균값 함수
+# (2일~X일 종가 모음) + 현재가격
+def MAL_Line(x):
+    sum = 0
+    for i in range(1, x):
+        sum = sum + End_Price[i]
+
+    sum_x = sum + Now_Price
+    MAL_Line_x = sum_x/x
+    return MAL_Line_x
 
 
 
-url2 = "http://m.finance.daum.net/m/item/item_daily.daum?code=035720"+"&page=2"
-html2 = requests.get(url2).text
-soup2 = BeautifulSoup(html2, "html.parser")
-test2 = soup2.select('tbody td.idx')
-array2 = [test2[i].text for i in range (len(test2))]
-print (array2)
-for i in range(len(array2)):
-    array2[i] =array2[i].replace(",","")
-print(array2[1])
+# 20일 이동평균값 (2일~20일 종가 모음) + 현재가격    *******다음 페이지 오류된 페이지 반영
+# Page1의 마지막 종가와 Page2의 첫번째 종가가 겹침 따라서 Page3의 첫번째 종가 합산
+def err_MAL_Line(x):
+    sum = 0
+    for i in range(1, x):
+        sum = sum + End_Price[i]
 
+    sum_x = sum + Now_Price - MAL_Function(2)[0] + MAL_Function(3)[0]
+    MAL_Line_x = sum_x/x
+    return MAL_Line_x
+
+
+
+if __name__ == "__main__":
+    print("TEST ")
+    print("현재가", Now_Price)
+    print("5일이동평균값", MAL_Line(5))
+    print("20일이동평균값", err_MAL_Line(20))
